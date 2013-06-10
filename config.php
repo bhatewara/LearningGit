2ffcdac72086122028dd491fe843b682ca322f9c
Rahul Bhatewara <bhatewara@gmail.com>
2013-06-10 11:42:35 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+echo "this is my configuration file, where configuration related to server settings will be stored.\n";
+echo "the content of this files will be different for every users, thus this file should not be committed back to repository\n";
+echo "this file should be ignored by git!";
+?>
